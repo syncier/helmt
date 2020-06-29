efab3ae5dbf4694f6b91da2ec07005f674267f5b
Torsten Walter <torsten.walter@syncier.com>
2020-06-29 21:11:52 +0200

    add version information
    Signed-off-by: Torsten Walter <torsten.walter@syncier.com>

--- cmd/root.go
+++ cmd/root.go
@@ -64,7 +64,8 @@ namespace and values are optional
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(version string) {
+	rootCmd.Version = version
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
